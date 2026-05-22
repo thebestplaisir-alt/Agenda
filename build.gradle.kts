0f8ccaa5e80eee3ba986a7bf42c9bc65b0ff4ded
Agenda Developer <agenda-user@example.com>
2026-05-22 10:42:38 +0200

    Fix KSP plugin version mismatch and update root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.ksp) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.jetbrains.compose) apply false
 }
